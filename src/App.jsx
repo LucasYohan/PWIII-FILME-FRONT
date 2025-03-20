f28db79aa7ed77701c08ce1913702f77817105f6
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-03-20 19:31:54 -0300

    Add cadastro de usuarios v2
    Adicao tela de cadastro de usuarios e de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Container from "./components/layout/Container/Container";
 import Home from "./components/pages/Home/Home";
 import Navbar from "./components/layout/Navbar/Navbar";
 import ListarFilme from "./components/pages/Listar_filme/ListarFilme";
 import CadastrarFilme from "./components/pages/Cadastrar_filme/CadastrarFilme";
 import CadastrarUsuario from "./components/pages/Cadastrar_usuario/Cadastrar_usuario";
-
+import Login from "./components/pages/Login/Login";
 
 function App() {
   return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Container>
-            <Routes>
-              <Route path="/" element={<Navigate to="/cadastrar_usuario" />} />
-              <Route path="/cadastrar_usuario" element={<CadastrarUsuario />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/listar_filme" element={<ListarFilme />} />
-              <Route path="/cadastrar_filme" element={<CadastrarFilme />} />
-            </Routes>
-          </Container>
-        </BrowserRouter>
-      </div>
-    </>
+    <BrowserRouter>
+      <MainLayout />
+    </BrowserRouter>
+  );
+}
+
+function MainLayout() {
+  const location = useLocation();
+  const hiddenNavbarRoutes = ["/login", "/cadastrar_usuario"];
+
+  return (
+    <Container>
+      {!hiddenNavbarRoutes.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Navigate to="login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastrar_usuario" element={<CadastrarUsuario />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/listar_filme" element={<ListarFilme />} />
+        <Route path="/cadastrar_filme" element={<CadastrarFilme />} />
+      </Routes>
+    </Container>
   );
 }
 
--- src/components/pages/Cadastrar_usuario/Cadastrar_usuario.jsx
+++ src/components/pages/Cadastrar_usuario/Cadastrar_usuario.jsx
@@ -2,8 +2,8 @@ import style from "./Cadastrar_usuario.module.css";
 import { useState } from "react";
 
 function Cadastrar_usuario() {
-  const [cadsUser, setCadseUser] = useState({
-    nome: "a",
+  const [cadsUser, setCadsUser] = useState({
+    nome: "",
     sobrenome: "",
     nome_usuario: "",
     email: "",
@@ -11,83 +11,93 @@ function Cadastrar_usuario() {
     telefone: "",
   });
 
-  function submit(event) {
-    event.preventDefault();
-    console.log(cadsUser);
+  function handleChange(e) {
+    setCadsUser({ ...cadsUser, [e.target.name]: e.target.value });
   }
 
+  function submit(event) {
+    event.preventDefault();
+    console.log("Dados enviados:", cadsUser);
+    alert("O usuário foi cadastrado com sucesso!");
+  }
+
+
+
   return (
-    <div>
-      <form onSubmit={submit}>
-        <div>
-          <label>Nome:</label>
+    <div className={style.container}>
+      <form className={style.form} onSubmit={submit}>
+        
+        <div className={style.inputGroup}>
+          <label className={style.label}>Nome:</label>
           <input
+            className={style.input}
+            name="nome"
             placeholder="Digite Nome"
-            value={nome}
-            onChange={(e) => {
-              changeUser.nome(e.target.value);
-            }}
+            value={cadsUser.nome}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <label>Sobrenome:</label>
+        <div className={style.inputGroup}>
+          <label className={style.label}>Sobrenome:</label>
           <input
+            className={style.input}
+            name="sobrenome"
             placeholder="Digite Sobrenome"
-            value={sobrenome}
-            onChange={(e) => {
-              changeUser.sobrenome(e.target.value);
-            }}
+            value={cadsUser.sobrenome}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <label>Nome de usuario:</label>
+        <div className={style.inputGroup}>
+          <label className={style.label}>Nome de usuário:</label>
           <input
-            placeholder="Digite Nome de usuario"
-            value={nome_usuario}
-            onChange={(e) => {
-              changeUser.nome_usuario(e.target.value);
-            }}
+            className={style.input}
+            name="nome_usuario"
+            placeholder="Digite Nome de usuário"
+            value={cadsUser.nome_usuario}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <label>Email:</label>
+        <div className={style.inputGroup}>
+          <label className={style.label}>Email:</label>
           <input
+            className={style.input}
+            name="email"
             placeholder="Digite Email"
-            value={email}
-            onChange={(e) => {
-              changeUser.email(e.target.value);
-            }}
+            type="email"
+            value={cadsUser.email}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <label>Senha:</label>
+        <div className={style.inputGroup}>
+          <label className={style.label}>Senha:</label>
           <input
+            className={style.input}
+            name="senha"
+            type="password"
             placeholder="Digite Senha"
-            value={senha}
-            onChange={(e) => {
-              changeUser.senha(e.target.value);
-            }}
+            value={cadsUser.senha}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <label>Telefone:</label>
+        <div className={style.inputGroup}>
+          <label className={style.label}>Telefone:</label>
           <input
+            className={style.input}
+            name="telefone"
             placeholder="Digite Telefone"
-            value={telefone}
-            onChange={(e) => {
-              changeUser.telefone(e.target.value);
-            }}
+            value={cadsUser.telefone}
+            onChange={handleChange}
           />
         </div>
 
-        <div>
-          <button type="submit">Login</button>
-        </div>
+        <button className={style.button} type="submit">
+          Cadastrar
+        </button>
       </form>
     </div>
   );
