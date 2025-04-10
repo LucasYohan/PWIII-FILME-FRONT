e9a0e04f6bf2b4c0febfdb91ab555d5f835e6d42
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-04-10 20:38:14 -0300

    Front correcao

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Container from "./components/layout/Container/Container";
 import Home from "./components/pages/Home/Home";
-import Navbar from "./components/layout/Navbar/Navbar";
 import ListarFilme from "./components/pages/Filmes/Listar_filme/ListarFilme";
 import CadastrarFilme from "./components/pages/Filmes/Cadastrar_filme/CadastrarFilme";
 import CadastrarUsuario from "./components/pages/Usuarios/Cadastrar_usuario/Cadastrar_usuario";
-import Login from "./components/pages/Usuarios/Login/Login";
+import Login from "./components/pages/Login/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <MainLayout />
-    </BrowserRouter>
-  );
-}
-
-function MainLayout() {
-  const location = useLocation();
-  const hiddenNavbarRoutes = ["/login", "/cadastrar_usuario"];
-
-  return (
-    <Container>
-      {!hiddenNavbarRoutes.includes(location.pathname) && <Navbar />}
+    <BrowserRouter className='body'>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
@@ -30,7 +16,7 @@ function MainLayout() {
         <Route path="/listar_filme" element={<ListarFilme />} />
         <Route path="/cadastrar_filme" element={<CadastrarFilme />} />
       </Routes>
-    </Container>
+    </BrowserRouter>
   );
 }
 
