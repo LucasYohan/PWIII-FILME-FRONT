14e3a2e6ce81fd84024a745df84ba91e627b9d32
Jaozinho759 <167478430+Jaozinho759@users.noreply.github.com>
2025-04-10 22:20:00 -0300

    Modificações de estilização na tela de Login

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import style from './Login.module.css'
+import style from './Login.module.css';
 import { FaEye as VisiblePassword } from "react-icons/fa";
 import { FaEyeSlash as HiddenPassword } from "react-icons/fa6";
 
 function Login() {
-
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const [passwordV, setPasswordV] = useState(false)
-    const [typePassword, setTypePassword] = useState('password')
+    const [passwordV, setPasswordV] = useState(false);
+    const [typePassword, setTypePassword] = useState('password');
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -30,33 +29,46 @@ function Login() {
     };
 
     const passwordVisible = () => {
-        setPasswordV(true)
-        setTypePassword('text')
-    }
+        setPasswordV(true);
+        setTypePassword('text');
+    };
 
     const passwordInvisible = () => {
-        setPasswordV(false)
-        setTypePassword('password')
-    }
+        setPasswordV(false);
+        setTypePassword('password');
+    };
 
     return (
         <div>
             <form onSubmit={handleLogin}>
-                <p> Usuarios:</p>
-                <input type="text" placeholder="Digite seu nome de usuario" value={name} onChange={(e) => setName(e.target.value)} />
+                <p>Usuário:</p>
+                <input
+                    type="text"
+                    placeholder="Digite seu nome de usuario"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
                 <p>Senha:</p>
-                <input type={typePassword} placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} />
-                {passwordV ? <VisiblePassword className={style.icon} onClick={passwordInvisible} /> : <HiddenPassword className={style.icon} onClick={passwordVisible} />}
+                <input
+                    type={typePassword}
+                    placeholder="Digite sua senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {passwordV ? (
+                    <VisiblePassword className={style.icon} onClick={passwordInvisible} />
+                ) : (
+                    <HiddenPassword className={style.icon} onClick={passwordVisible} />
+                )}
                 <div className={style.buttonLogin}>
-                    <button type="submit">Login</button>
+                    <button className={style.botao} type="submit">Login</button>
+                    <Link to="/cadastrar_usuario">
+                <button className={style.botao_cadastro}>Cadastrar Usuário</button>
+            </Link>
                 </div>
             </form>
-            <Link to="/cadastrar_usuario">
-                <button>cadastrar_usuario</button>
-            </Link>
         </div>
-    )
-
+    );
 }
 
 export default Login;
